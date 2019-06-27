70e73b5a41b3318f518b5d0e9bfdb1872f24def7
Francisco Castellanos <fvcastellanos@gmail.com>
2019-06-27 02:01:30 -0600

    initial commit

---
+++ src/main/java/com/dorefactor/rd/api/RdApiApplication.java
@@ -0,0 +1,13 @@
+package com.dorefactor.rd.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dorefactor/rd/api/RdApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dorefactor.rd.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RdApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
